171776083ad9efbd46ee50874af5cc276617861d
Tran Quoc Lap <quoclap.tran@gmail.com>
2016-11-21 10:21:06 +0700

    Them vao Todo

--- Todo.md
+++ Todo.md
@@ -1,6 +1,8 @@
 - Đưa các chương trình trong file entry.js vào một hàm trên Lambda nhằm đảm bảo tính đóng của chương trình.
 - Tạo layout cho web app gồm tất cả các function cần thiết. có thể tham khảo layout từ các nguồn khác.
 - Tạo các hàm Lambda cập nhật vào các database của ngày, tuần, tháng, năm với trigger source có thể là từ phía khách hàng, từ phía công ty, từ DynamoDB Stream.
+	+ Các hàm Lambda dùng để trigger các function query dữ liệu database và cập nhật dữ liệu cho các bảng đảm bảo tính đóng.
+	+ Trên web app nhận dữ liệu từ các hàm Lambda, thực hiện chức năng chủ yếu là hiển thị sử dụng các thư viện như Chart.js, React Native , ...
 - Tạo một cơ chế để Log tất cả các event liên quan đến hệ thống, từ các thông số thụ động của các thiết bị cũng như các actions được tạo ra từ các rule, các command từ user.
 - Tạo cơ chế phản hồi từ khách hàng: khách hàng có thể gửi về công ty các yêu cầu tuỳ biến về hệ thống cũng như giao diện, về phía công ty có thể đưa ra trước các example để khách hàng dựa vào đó để có những đề xuất chỉnh sửa, muốn có nhiều ý kiến phản hồi nâng cao chất lượng sản phẩm và dịch vụ thì cũng nên đầu tư thời gian tạo ra nhiều example cho mỗi đối tượng khách hàng.
 - Tạo cơ chế lưu cookies của mỗi senssion , User mỗi lần đăng nhâp vô app sẽ được lưu các thông tin tạm thời về username password, token...
--- src/entry.js
+++ src/entry.js
@@ -111,55 +111,8 @@ var userData = {
 var dynamodb = null;//Tao bien toan cuc
 var docClient = null;
 var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
-cognitoUser.authenticateUser(authenticationDetails, {
-    onSuccess: function (result) {
-      // /console.log('access token + ' + result.getAccessToken().getJwtToken());
-      AWS.config.region = 'ap-northeast-1'; // Region
-      AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-          IdentityPoolId : config.IdentityPoolId,
-          Logins:{
-            'cognito-idp.ap-northeast-1.amazonaws.com/ap-northeast-1_laJ43mEoj' : result.getIdToken().getJwtToken()
-          }
-      });
 
-      //Using DynamoDB DOCUMENT SDK
-      docClient = new AWS.DynamoDB.DocumentClient();
-      queryDatabase();
-      setInterval(queryDatabase,300000);
-    },
-    onFailure: function(err) {
-        alert(err);
-    }
-});
-
-function queryDatabase(){
-  ts = new Date().getTime();
-  var params = {
-    TableName: config.tableName,
-    ScanIndexForward: false,
-    ProjectionExpression: "#Values",
-    KeyConditionExpression: "#Id = :id AND #Time <= :time",
-    ExpressionAttributeNames:{
-      "#Id" : "Id",
-      "#Time" : "Timestamp",
-      "#Values" : "Values"
-    },
-    ExpressionAttributeValues:{
-      ":id" : "ph-smart-001",
-      ":time": ts
-    },
-    Limit: 15,
-    ReturnConsumedCapacity: 'TOTAL'
-  };
-  docClient.query(params,function(err, data){
-    if(err) console.log(err, err.stack);
-    else listData(data);
-  });
-
-}
-
-function listData(data){
-  //console.log("Data: " + JSON.stringify(data));
+//variables for drawing chart
   var recentEventsDateTime = [];
   var recentEventsCounter = [];
   var codData = [];
@@ -171,33 +124,8 @@ function listData(data){
   var tempData = [];
   var dateHour;
   var cod;
-  data.Items.forEach(function(item) {
-      dateHour = JSON.stringify(item.Values.Time);
-      //recentEventsDateTime.push(dateHour.slice(1, -4));
-      recentEventsDateTime.push(item.Values.Time);
-      codData.push(item.Values.COD);
-      bodData.push(item.Values.BOD);
-      doData.push(item.Values.DO);
-      phData.push(item.Values.pH);
-      colorData.push(item.Values.Color);
-      condData.push(item.Values.Cond);
-      tempData.push(item.Values.Temp);
-    });
-    //Chart.js code
-    //Create random values
-    var randomScalingFactor = function() {
-        return Math.round(Math.random() * 100 * (Math.random() > 0.5 ? -1 : 1));
-    };
-    var randomColorFactor = function() {
-        return Math.round(Math.random() * 255);
-    };
-    var randomColor = function(opacity) {
-        return 'rgba(' + randomColorFactor() + ',' + randomColorFactor() + ',' + randomColorFactor() + ',' + (opacity || '.3') + ')';
-    };
-
-    var ctx = document.getElementById("myChart").getContext("2d");
-    var background = randomColor(0.5);
-    var myChart = new Chart(ctx, {
+  var ctx = document.getElementById("myChart").getContext("2d");
+  var myChart = new Chart(ctx, {
           type: 'bar',
           data: {
             labels: recentEventsDateTime,
@@ -274,5 +202,158 @@ function listData(data){
             }
           }
       });
+
+
+cognitoUser.authenticateUser(authenticationDetails, {
+    onSuccess: function (result) {
+      // /console.log('access token + ' + result.getAccessToken().getJwtToken());
+      AWS.config.region = 'ap-northeast-1'; // Region
+      AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+          IdentityPoolId : config.IdentityPoolId,
+          Logins:{
+            'cognito-idp.ap-northeast-1.amazonaws.com/ap-northeast-1_laJ43mEoj' : result.getIdToken().getJwtToken()
+          }
+      });
+
+      //Using DynamoDB DOCUMENT SDK
+      docClient = new AWS.DynamoDB.DocumentClient();
+      queryDatabase();
+      setInterval(queryDatabase,10000);
+    },
+    onFailure: function(err) {
+        alert(err);
+    }
+});
+
+function queryDatabase(){
+  ts = new Date().getTime();
+  var params = {
+    TableName: config.tableName,
+    ScanIndexForward: false,
+    ProjectionExpression: "#Values",
+    KeyConditionExpression: "#Id = :id AND #Time <= :time",
+    ExpressionAttributeNames:{
+      "#Id" : "Id",
+      "#Time" : "Timestamp",
+      "#Values" : "Values"
+    },
+    ExpressionAttributeValues:{
+      ":id" : "ph-smart-001",
+      ":time": ts
+    },
+    Limit: 15,
+    ReturnConsumedCapacity: 'TOTAL'
+  };
+  docClient.query(params,function(err, data){
+    if(err) console.log(err, err.stack);
+    else listData(data);
+  });
+
+}
+
+function listData(data){
+  data.Items.forEach(function(item) {
+      dateHour = JSON.stringify(item.Values.Time);
+      //recentEventsDateTime.push(dateHour.slice(1, -4));
+      recentEventsDateTime.push(item.Values.Time);
+      codData.push(item.Values.COD);
+      bodData.push(item.Values.BOD);
+      doData.push(item.Values.DO);
+      phData.push(item.Values.pH);
+      colorData.push(item.Values.Color);
+      condData.push(item.Values.Cond);
+      tempData.push(item.Values.Temp);
+    });
+    //Chart.js code
+    //Create random values
+    var randomScalingFactor = function() {
+        return Math.round(Math.random() * 100 * (Math.random() > 0.5 ? -1 : 1));
+    };
+    var randomColorFactor = function() {
+        return Math.round(Math.random() * 255);
+    };
+    var randomColor = function(opacity) {
+        return 'rgba(' + randomColorFactor() + ',' + randomColorFactor() + ',' + randomColorFactor() + ',' + (opacity || '.3') + ')';
+    };
+    myChart.clear();
+    myChart.update();
+    // var ctx = document.getElementById("myChart").getContext("2d");
+    // var myChart = new Chart(ctx, {
+    //       type: 'bar',
+    //       data: {
+    //         labels: recentEventsDateTime,
+    //         datasets: [{
+    //             type: 'line',
+    //             label: "COD",
+    //             data:codData,
+    //             backgroundColor: "rgba(250,0,0,0.8)",
+    //             borderColor: "rgba(250,0,0,0.8)",
+    //             fill: false,
+    //             //borderDash: [5, 5],
+    //         }, {
+    //             type: 'line',
+    //             label: "BOD",
+    //             data: bodData,
+    //             borderColor: "rgba(0,250,0,0.3)",
+    //             fill: false,
+    //             //borderDash: [5, 5],
+    //         }, {
+    //             //type: 'bar',
+    //             label: "DO",
+    //             data: doData,
+    //             borderColor: "rgba(0,0,255,0.3)",
+    //             backgroundColor: "rgba(150,200,0,0.3)",
+    //             //lineTension: 0,
+    //             fill: false,
+    //         },{
+    //             //type: 'bar',
+    //             label: "Conductivity",
+    //             data: condData,
+    //             backgroundColor: "rgba(0,0,254,0.6)",
+    //             fill: false,
+    //         },{
+    //             //type: 'bar',
+    //             label: "Temperature",
+    //             data: tempData,
+    //             backgroundColor: "rgba(250,0,0,0.3)",
+    //             fill: false,
+    //         },{
+    //             //type: 'bar',
+    //             label: "Color",
+    //             data: colorData,
+    //             backgroundColor: "rgba(150,0,180,0.3)",
+    //             fill: false,
+    //         },
+    //         {
+    //             //type: 'bar',
+    //             label: "pH",
+    //             data: phData,
+    //             fill: false,
+    //         }]
+    //     },
+    //     options: {
+    //         responsive: true,
+    //         scales: {
+    //             xAxes: [{
+    //                 display: true,
+    //                 scaleLabel: {
+    //                     display: true,
+    //                     labelString: 'Time'
+    //                 }
+    //             }],
+    //             yAxes: [{
+    //                 display: true,
+    //                 scaleLabel: {
+    //                     display: true,
+    //                     labelString: 'Value'
+    //                 }
+    //             }]
+    //         },
+    //         title: {
+    //             display: true,
+    //             text: 'Multi parameters Online monitoring System.'
+    //         }
+    //       }
+    //   });
 
 }
